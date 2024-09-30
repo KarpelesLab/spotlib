b1f73c7b911d4307c6be367f8f65d7bd382de382
Mark Karpeles <magicaltux@gmail.com>
2024-09-30 18:48:29 +0900

    add handler for update

--- defhandlers.go
+++ defhandlers.go
@@ -1,9 +1,12 @@
 package spotlib
 
 import (
+	"context"
 	"fmt"
 	"runtime"
+	"time"
 
+	"github.com/KarpelesLab/emitter"
 	"github.com/KarpelesLab/spotproto"
 )
 
@@ -21,5 +24,11 @@ func (c *Client) setDefaultHandlers() {
 	})
 	c.SetHandler("finger", func(msg *spotproto.Message) ([]byte, error) {
 		return c.idBin, nil
+	})
+	c.SetHandler("check_update", func(msg *spotproto.Message) ([]byte, error) {
+		ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+		defer cancel()
+		emitter.Global.Emit(ctx, "check_update")
+		return nil, nil
 	})
 }
